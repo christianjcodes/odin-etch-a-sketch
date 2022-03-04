55d105108ec27963bf993f6394bf888de9964016
Christian Johnson <cartian4@gmail.com>
2022-03-03 20:30:37 -0500

    Continue coding rows and columns

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function createRow(rowNum) {
     for (let i = 0; i < rowNum; i++) {
         let row = document.createElement("div");
+        container.appendChild(row);
+        row.className = "row";
     }
 }
 
 function createColumn(columnNum) {
     for (let i = 0; i < columnNum; i++) {
-        
+
     }
 }
 
