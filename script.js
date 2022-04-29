074b429c7a2f3b4267362bbd6dfa64a0e47ae024
Christian Johnson <cartian4@gmail.com>
2022-04-28 20:39:46 -0400

    work on createColumn and createRow functions

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
+const container = document.getElementById("container");
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+defaultGrid();
+
+function defaultGrid() {
+    createRow(16);
+    createColumn(16);
+}
+
 function createRow(rowNum) {
     for (let i = 0; i < rowNum; i++) {
         let row = document.createElement("div");
-        container.appendChild(row);
-        row.className = "row";
+        container.appendChild(row).className = "gridRow";
     }
 }
 
-function createColumn(columnNum) {
+function createColumn(cellNum) {
     for (let i = 0; i < rows.length; i++) {
-        for (j = 0; j < columnNum; j++) {
+        for (let j = 0; j < cellNum; j++) {
             let newCell = document.createElement("div");
             rows[j].appendChild(newCell).className = "cell";
         }
     }
 }
-
